188b1c5b25d6225390d081054448eb9ab211dacb
Dan Kaplun <dbkaplun@gmail.com>
2015-06-27 13:01:44 -0400

    fix(test): add rows/columns to stub for stdout

--- spec/util.js
+++ spec/util.js
@@ -5,13 +5,16 @@ var blessed = require('blessed');
 var through2 = require('through2');
 
 module.exports = {
-  stdioFactory: function () {
+  readStreamFactory: function () {
     return _.merge(through2(), {setRawMode: _.noop});
+  },
+  writeStreamFactory: function () {
+    return _.merge(through2(), {rows: 24, columns: 80});
   },
   screenFactory: function (opts) {
     return new blessed.screen(_.merge({
-      input: module.exports.stdioFactory(),
-      output: module.exports.stdioFactory(),
+      input: module.exports.readStreamFactory(),
+      output: module.exports.writeStreamFactory()
     }, opts));
   }
 };
